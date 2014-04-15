200a618e91e83c1f8c51098c89d10c30c6f75012
Matthew Brisebois <matthew@webheroes.ca>
2014-04-15 17:53:26 -0600

    Added many string methods

--- js/pythonify-tests.js
+++ js/pythonify-tests.js
@@ -147,6 +147,214 @@
     // test list.sort()
     assert( l.sort()[0] == 'girls' )
 
+    s = "Hello World"
+
+    // test string.repeat()
+    assert( s.repeat(2) == s+s )
+
+    // test string.capitalize()
+    assert( "this will have ONLY the first letter capped.".capitalize() == "This will have only the first letter capped." )
+
+    // test string.endswith()
+    assert( s.endswith('rld') )
+    assert( ! s.endswith('llo') )
+    assert( s.endswith('rld', 5) )
+    assert( ! s.endswith('rld', 9) )
+    assert( s.endswith('Wor', 6, -2) )
+    assert( ! s.endswith('Wor', 6, -3) )
+
+    // test string.count()
+    assert( s.count('H') == 1 )
+    assert( s.count('h') == 0 )
+    assert( s.count('l') == 3 )
+    assert( s.count('l', 3) == 2 )
+    assert( s.count('l', -8, -4) == 1 )
+
+    // test string.center()
+    assert( s.center(17) == "   "+s+"   " )
+    assert( s.center(17, ".") == "..."+s+"..." )
+    assert( s.center(18, ".") == "...."+s+"..." )
+
+    // test string.replaceAt()
+    assert( s.replaceAt(6, "   ") == "Hello   World" )
+    assert( s.replaceAt(6, "   ", 2) == "Hello   orld" )
+    assert( s.replaceAt(11, ".") == "Hello Worl." )
+
+    // test string.expandtabs()
+    assert( "Hello\tWorld".expandtabs() == "Hello   World" )
+    assert( "Hello\tWorld".expandtabs(7) == "Hello  World" )
+    assert( "  Hello\tWorld".expandtabs() == "  Hello World" )
+    assert( "  \tHello\tWorld\t".expandtabs() == "        Hello   World   " )
+    assert( "  \tHello\tWorld\t".expandtabs(4) == "    Hello   World   " )
+    assert( "\tHello\n\tWorld".expandtabs(4) == "    Hello\n    World" )
+    assert( "\tHello\t\n\tWorld".expandtabs(4) == "    Hello   \n    World" )
+
+    // test string.find()
+    assert( s.find('lo') == 3 )
+    assert( s.find('los') == -1 )
+    assert( s.find('l') == 2 )
+    assert( s.find('l', 2) == 0 )
+    assert( s.find('rl', 0, -2) == -1 )
+
+    // test string.index()
+    var error;
+    try {
+        s.index('los');
+    }
+    catch(err) {
+        error = err
+    }
+    assert( error == "ValueError" )
+    assert( s.index('rl') == 8 )
+
+    // test string.format()
+    assert( "{0} {1}".format("Hello", "World") == "Hello World" )
+    assert( "{length} {0} {text} {1}".format("Hello", "World", { length: 4, text: 'Flappy Bird' }) == "4 Hello Flappy Bird World" )
+    assert( "{text} {length}".format({ length: 4, text: 'Flappy Bird' }) == "Flappy Bird 4" )
+
+    // test string.isalnum()
+    assert( "asldfkj8433".isalnum() === true )
+    assert( "a;192".isalnum() === false )
+    assert( "".isalnum() === false )
+
+    // test string.isalpha()
+    assert( "asldfkj".isalpha() === true )
+    assert( "a;192".isalpha() === false )
+    assert( "".isalpha() === false )
+
+    // test string.isdigit()
+    assert( "819".isdigit() === true )
+    assert( "a;192".isdigit() === false )
+    assert( "".isdigit() === false )
+
+    // test string.islower()
+    assert( "all lower case".islower() === true )
+    assert( "All Lower Case".islower() === false )
+    assert( "".islower() === false )
+
+    // test string.isupper()
+    assert( "ALL LOWER CASE".isupper() === true )
+    assert( "All Lower Case".isupper() === false )
+    assert( "".isupper() === false )
+ 
+    // test string.isspace()
+    assert( " \t\n\r\v\f".isspace() === true )
+    assert( " \ta".isspace() === false )
+    assert( "".isspace() === false )
+
+    var d = { one: "Hello", two: "World" };
+
+    // test string.join()
+    assert( ", ".join(d) == "one, two" )
+    assert( ", ".join(d.values()) == "Hello, World" )
+    assert( " ".join(d) == "one two" )
+    assert( " ".join(d.values()) == "Hello World" )
+
+    // test string.ljust()
+    assert( "Hey!".ljust(10) == "Hey!      " )
+    assert( "Hey!".ljust(10, ".") == "Hey!......" )
+    assert( "Hey!".ljust(4) == "Hey!" )
+
+    // test string.lower()
+    assert( "HEY".lower() == "hey" )
+
+    // test string.lstrip()
+    assert( '    spacious    '.lstrip() == "spacious    " )
+    assert( 'www.example.com'.lstrip('cmowz.') == "example.com" )
+
+    // test string.partition()
+    assert( 'Hello World'.partition(" ")[0] == 'Hello' )
+    assert( 'Hello World'.partition(" ")[1] == ' ' )
+    assert( 'Hello World'.partition(" ")[2] == 'World' )
+
+    // test string.replace()
+    assert( '    '.replace(" ", ".", 2) == "..  " )
+
+    // test string.rfind()
+    assert( s.rfind('l') == 9 )
+    assert( s.rfind('ls') == -1 )
+    assert( s.rfind('l', 0, -2) == 3 )
+    assert( s.rfind('ld', 0, -2) == -1 )
+
+    // test string.index()
+    var error;
+    try {
+        s.rindex('los');
+    }
+    catch(err) {
+        error = err
+    }
+    assert( error == "ValueError" )
+    assert( s.rindex('ld') == 9 )
+
+    // test string.rpartition()
+    assert( 'Hello World Smack'.rpartition(" ")[0] == 'Hello World' )
+    assert( 'Hello World Smack'.rpartition(" ")[1] == ' ' )
+    assert( 'Hello World Smack'.rpartition(" ")[2] == 'Smack' )
+
+    // test string.rjust()
+    assert( "Hey!".rjust(10) == "      Hey!" )
+    assert( "Hey!".rjust(10, ".") == "......Hey!" )
+    assert( "Hey!".rjust(4) == "Hey!" )
+
+    // test string.rstrip()
+    assert( '    spacious    '.rstrip() == "    spacious" )
+    assert( 'www.example.com'.rstrip('cmowz.') == "www.example" )
+
+    // test string.split()
+    assert( len('10.0.1.15'.split('.')) == 4 )
+    assert( len('10.0.1.15'.split('.', 2)) == 3 )
+    assert( ''.split(' ')[0] == '' )
+    assert( len(''.split()) == 0 )
+    assert( len('Smack That'.split()) == 2 )
+
+    // test string.splitlines()
+    assert( len('10\n0\n1\n15'.splitlines())		== 4 )
+    assert( len('10\n0\n1\n15'.splitlines(true))	== 4 )
+    assert( '10\n0\n1\n15'.splitlines()[0]		== "10" )
+    assert( '10\n0\n1\n15'.splitlines(true)[0]		== "10\n" )
+
+    // test string.startswith()
+    assert( s.startswith('Hel') )
+    assert( ! s.startswith('ell') )
+    assert( s.startswith('lo ', 3) )
+    assert( ! s.startswith('or', 9) )
+    assert( s.startswith('Wor', 6, -2) )
+    assert( ! s.startswith('World', 6, -2) )
+
+    // test string.strip()
+    assert( '    spacious    '.strip() == "spacious" )
+    assert( 'www.example.com'.strip('cmowz.') == "example" )
+
+    // test string.swapcase()
+    assert( 'SwApCaSe'.swapcase() == "sWaPcAsE" )
+
+    // test string.title()
+    assert( "they're bill's friends.".title() == "They're Bill's Friends." )
+
+    // test string.upper()
+    assert( "hey".upper() == "HEY" )
+
+    // test string.zfill()
+    assert( "89".zfill(5) == "00089" )
+    assert( "-89".zfill(5) == "-0089" )
+    assert( "bill".zfill(8) == "0000bill" )
+    assert( "-bill".zfill(8) == "-000bill" )
+    
+
+
     console.log("Passed all "+passes+" tests");
+
+    iterrange = generator(function( start, stop ) {
+        var current	= this.count + start;
+        if( current <= stop ) {
+            return current;
+        }
+        else {
+            return undefined;
+        }
+    });
+    range = iterrange(11, 30);
+    console.log(["range", range])
 
 })(window);
--- js/pythonify.js
+++ js/pythonify.js
@@ -85,7 +85,6 @@
         return a
     }
     
-    // Array changes must be done before Object changes
     Object.defineProperties( Array.prototype, {
         "append": {
             writable: true,
@@ -184,7 +183,359 @@
             value: _notIn
         }
     });
+
+    String.ascii_letters	= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    String.ascii_lowercase	= "abcdefghijklmnopqrstuvwxyz"
+    String.ascii_uppercase	= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    String.ascii_digits		= "0123456789"
+    String.ascii_hexdigits	= "0123456789abcdefABCDEF"
+    String.ascii_octdigits	= "01234567"
+    String.ascii_letters	= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    String.punctuation		= "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+    String.printable		= "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"
+    String.whitespace		= "\t\n\x0b\x0c\r "
+    Object.defineProperties( String.prototype, {
+        "repeat": {
+            writable: true,
+            value: function(len) {
+                return new Array( len + 1 ).join( this );
+            }
+        },
+        "capitalize": {
+            writable: true,
+            value: function() {
+                return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase()
+            }
+        },
+        "center": {
+            writable: true,
+            value: function(width, fillchar) {
+                if( this.length >= width ) {
+                    return this;
+                }
+                var fillchar	= fillchar ? fillchar : " ";
+                var blen	= Math.ceil(  (width - this.length)/2 );
+                var elen	= Math.floor( (width - this.length)/2 );
+                var bstr	= new Array( blen + 1 ).join( fillchar );
+                var estr	= new Array( elen + 1 ).join( fillchar );
+                return bstr + this + estr;
+            }
+        },
+        "count": {
+            writable: true,
+            value: function(sub, start, end) {
+                return this.slice(start, end).split(sub).length - 1;
+            }
+        },
+        "endswith": {
+            writable: true,
+            value: function(suffix, start, end) {
+                var str		= this.slice(start, end)
+                , needles	= typeof suffix == 'string' ? [suffix] : suffix;
+                for( i in needles ) {
+                    var n	= needles[i];
+                    if( n.length > str.length ) {
+                        continue;
+                    }
+                    if( n == str.substring( str.length - n.length ) ) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        },
+        "replaceAt": {
+            writable: true,
+            value: function(index, str, length) {
+                var i		= index - 1;
+                return this.substring(0, i) + str + this.substring( i + (length ? length : 1) );
+            }
+        },
+        "expandtabs": {
+            writable: true,
+            value: function(tabsize) {
+                var results	= [];
+                var tabsize	= tabsize === undefined ? 8 : tabsize;
+                var lines	= this.split("\n");
+                for( n in lines ) {
+                    var line	= lines[n];
+                    while( line.find("\t") != -1 ) {
+                        var i	= line.find("\t");
+                        line	= line.replaceAt( i+1, " ".repeat( tabsize - (i % tabsize) ) );
+                    }
+                    results.append(line);
+                }
+                return results.join("\n");
+            }
+        },
+        "find": {
+            writable: true,
+            value: function( sub, start, end ) {
+                return this.slice(start, end).indexOf( sub );
+            }
+        },
+        "format": {
+            writable: true,
+            value: function() {
+                var str		= this;
+                for( var i=0; i < arguments.length; i++ ) {
+                    var arg	= arguments[i];
+                    if( is_dict(arg) ) {
+                        for( k in arg ) {
+                            var re	= new RegExp( RegExp.escape("{"+k+"}") );
+                            str		= str.replace(re, arg[k]);
+                        }
+                    }
+                    else {
+                        var re	= new RegExp( RegExp.escape("{"+i+"}") );
+                        str	= str.replace(re, arg);
+                    }
+                }
+                return str;
+            }
+        },
+        "index": {
+            writable: true,
+            value: function( sub, start, end ) {
+                var r		= this.find(sub, start, end);
+                if( r === -1 ) {
+                    throw "ValueError";
+                }
+                return r;
+            }
+        },
+        "isalnum": {
+            writable: true,
+            value: function() {
+                return this.match( /^[a-z0-9]+$/i ) ? true : false;
+            }
+        },
+        "isalpha": {
+            writable: true,
+            value: function() {
+                return this.match( /^[a-z]+$/i ) ? true : false;
+            }
+        },
+        "isdigit": {
+            writable: true,
+            value: function() {
+                return this.match( /^[0-9]+$/ ) ? true : false;
+            }
+        },
+        "islower": {
+            writable: true,
+            value: function() {
+                return this.match( /^[a-z ]+$/ ) ? true : false;
+            }
+        },
+        "isupper": {
+            writable: true,
+            value: function() {
+                return this.match( /^[A-Z ]+$/ ) ? true : false;
+            }
+        },
+        "isspace": {
+            writable: true,
+            value: function() {
+                return this.match( /^[\s]+$/ ) ? true : false;
+            }
+        },
+        "join": {
+            writable: true,
+            value: function( iter ) {
+                return iter.join( this );
+            }
+        },
+        "ljust": {
+            writable: true,
+            value: function( width, fillchar ) {
+                var fillchar	= fillchar ? fillchar : " ";
+                return this + fillchar.repeat( width - this.length );
+            }
+        },
+        "lower": {
+            writable: true,
+            value: function() {
+                return this.toLowerCase();
+            }
+        },
+        "lstrip": {
+            writable: true,
+            value: function(chars) {
+                var chars	= chars ? chars : " ";
+                var re		= new RegExp( "^["+chars+"]*" );
+                return this.replace(re, "");
+            }
+        },
+        "partition": {
+            writable: false,
+            value: function( sep ) {
+                if( sep === undefined ) {
+                    throw "TypeError: partition() takes exactly one argument ("+arguments.length+" given)";
+                }
+                var index	= this.find(sep);
+                return index
+                    ? [ this.slice( 0, index ), sep, this.slice( index+1 ) ]
+                    : [ this, "", "" ];
+            }
+        },
+        "_replace": {
+            writable: false,
+            value: String.prototype.replace
+        },
+        "replace": {
+            writable: true,
+            value: function(pattern, replace, count) {
+                var str		= this;
+                var re		= new RegExp( pattern );
+                if( count !== undefined ) {
+                    var re	= new RegExp( re.source, [re.ignoreCase ? "i" : "", re.multiline ? "m" : ""].join("") );
+                    for( var i=0; i < count; i++ ) {
+                        str	= str._replace(re, replace);
+                    }
+                }
+                else {
+                    str		= str._replace(re, replace);
+                }
+                return str;
+            }
+        },
+        "rfind": {
+            writable: true,
+            value: function( sub, start, end ) {
+                return this.slice(start, end).lastIndexOf( sub );
+            }
+        },
+        "rindex": {
+            writable: true,
+            value: function( sub, start, end ) {
+                var r		= this.rfind(sub, start, end);
+                if( r === -1 ) {
+                    throw "ValueError";
+                }
+                return r;
+            }
+        },
+        "rpartition": {
+            writable: false,
+            value: function( sep ) {
+                if( sep === undefined ) {
+                    throw "TypeError: partition() takes exactly one argument ("+arguments.length+" given)";
+                }
+                var index	= this.rfind(sep);
+                return index
+                    ? [ this.slice( 0, index ), sep, this.slice( index+1 ) ]
+                    : [ this, "", "" ];
+            }
+        },
+        "rjust": {
+            writable: true,
+            value: function( width, fillchar ) {
+                var fillchar	= fillchar ? fillchar : " ";
+                return fillchar.repeat( width - this.length ) + this;
+            }
+        },
+        "rstrip": {
+            writable: true,
+            value: function(chars) {
+                var chars	= chars ? chars : " ";
+                var re		= new RegExp( "["+chars+"]*$" );
+                return this.replace(re, "");
+            }
+        },
+        "_split": {
+            writable: true,
+            value: String.prototype.split
+        },
+        "split": {
+            writable: true,
+            value: function( sep, maxsplit ) {
+                var maxsplit	= maxsplit === undefined ? maxsplit : maxsplit + 1;
+                var splits	= this._split( sep || ' ', maxsplit );
+                return sep === undefined && len(splits) == 1 && splits[0] == "" ? [] : splits;
+            }
+        },
+        "splitlines": {
+            writable: true,
+            value: function( keepends ) {
+                var splits	= this._split("\n");
+                if( keepends === true ) {
+                    for( i in splits ) {
+                        splits[i]      += "\n"
+                    }
+                }
+                return splits;
+            }
+        },
+        "startswith": {
+            writable: true,
+            value: function(suffix, start, end) {
+                var str		= this.slice(start, end)
+                , needles	= typeof suffix == 'string' ? [suffix] : suffix;
+                for( i in needles ) {
+                    var n	= needles[i];
+                    if( n.length > str.length ) {
+                        continue;
+                    }
+                    if( n == str.substring( 0, n.length ) ) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        },
+        "strip": {
+            writable: true,
+            value: function(chars) {
+                return this.lstrip(chars).rstrip(chars);
+            }
+        },
+        "swapcase": {
+            writable: true,
+            value: function(chars) {
+                var str		= this;
+                for( var i=0; i < str.length; i++ ) {
+                    var c	= str.charAt(i);
+                    if( c === c.upper() ) {
+                        str	= str.replaceAt(i+1, c.lower());
+                    }
+                    else {
+                        str	= str.replaceAt(i+1, c.upper());
+                    }
+                }
+                return str;
+            }
+        },
+        "title": {
+            writable: true,
+            value: function() {
+                var words	= this.split(' ');
+                for( i in words ) {
+                    words[i]	= words[i].capitalize();
+                }
+                return words.join(' ');
+            }
+        },
+        "upper": {
+            writable: true,
+            value: function() {
+                return this.toUpperCase();
+            }
+        },
+        "zfill": {
+            writable: true,
+            value: function( width ) {
+                var sign	= this.startswith('-');
+                return (sign ? '-' : '') + this.slice( sign ? 1 : 0 ).rjust( sign ? width-1 : width, '0' );
+            }
+        }
+    });
+
+    RegExp.escape = function(str) {
+        return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    }
     
+    // Object property definitions must be done last
     Object.defineProperties( Object.prototype, {
         "get": {
             writable: true,
@@ -276,11 +627,61 @@
                 }
                 return null
             }
+        },
+        "join": {
+            writable: true,
+            value: function( str ) {
+                return this.keys().join( str );
+            }
         }
     });
+
+    // usage:
+    // 
+    // range = generator(function( start, stop ) {
+    //     if( this.count + start < stop )
+    //         return this.count++;
+    //     else
+    //         return null;
+    // });
+    // 
+    function generator( fn, args ) {
+        if( !( this instanceof generator )) {
+            return new generator( fn, args );
+        }
+
+        console.log(['generator', fn])
+        this._fn	= fn;
+        var t		= this;
+        if( args ) {
+
+            this._args	= args;
+            this.count	= 0;
+            return this;
+        }
+        else {
+            return function () {
+                return t.create.apply( t, arguments )
+            }
+        }
+    }
+    generator.prototype.create = function() {
+        console.log(['create', this._fn])
+        return new generator( this._fn, arguments );
+    }
+    generator.prototype.next = function() {
+        var ret = this._fn.apply( this, this._args );
+        this.count++;
+        return ret;
+    }
+    generator.prototype.add_arg = function( item ) {
+        [].push.call( this._args, item );
+        return this._args.length;
+    }
 
     window.len		= len
     window.list		= list
     window.dict		= dict
+    window.generator	= generator
 
 })(window);
